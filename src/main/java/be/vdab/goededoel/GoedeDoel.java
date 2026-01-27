4c4cca7d703c95167983da46cdaba8f6d67b1cfb
LabJavaUser <zokasha1@hotmail.com>
2026-01-27 12:14:26 +0100

    goededoel class en test scrijven

---
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -0,0 +1,19 @@
+package be.vdab.goededoel;
+
+import java.math.BigDecimal;
+
+public class GoedeDoel {
+    private final String naam;
+    private BigDecimal opbrengst;
+
+    public GoedeDoel(String naam) {
+        this.naam = naam;
+    }
+    public String getNaam() {
+        return naam;
+    }
+    public BigDecimal getOpbrengst() {
+        return opbrengst;
+    }
+
+}
---
+++ src/test/java/goededoel/GoedeDoelTest.java
@@ -0,0 +1,18 @@
+package goededoel;
+import be.vdab.goededoel.GoedeDoel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class GoedeDoelTest {
+    private static final String NAAM="CliniClowns";
+    private GoedeDoel goedeDoel;
+    @BeforeEach
+    void beforeEach() {
+        goedeDoel=new GoedeDoel(NAAM);
+    }
+    @Test
+    void getNaam() {
+        assertThat(goedeDoel.getNaam()).isEqualTo(NAAM);
+    }
+}
