c7ff5a5d65532661f97cea89928bcd6707e97117
LabJavaUser <zokasha1@hotmail.com>
2026-01-30 10:44:57 +0100

    spotbugs maven plugin

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -2,17 +2,31 @@ package be.vdab.goededoel;
 
 import java.math.BigDecimal;
 import java.util.Objects;
-
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author Joe Dalton
+ */
 public class GoedeDoel {
     private final String naam;
     private  BigDecimal opbrengst=BigDecimal.ZERO;
-
+    /**
+     * Maakt een GoedeDoel object
+     * @param naam De naam van het goede doel
+     */
     public GoedeDoel(String naam) {
         this.naam = naam;
     }
+    /**
+     * Geeft de naam terug
+     * @return de naam
+     */
     public String getNaam() {
         return naam;
     }
+    /**
+     * Geeft de opbrengst terug
+     * @return de opbrengst
+     */
     public BigDecimal getOpbrengst() {
         return opbrengst;
     }
